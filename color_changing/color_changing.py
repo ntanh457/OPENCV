44eaa5b15acca622a817cf186f1d39d5f97196b9
Anh Tuan Nguyen <ntanh457@gmail.com>
2020-07-04 00:51:37 -0500

    anh edit 7/4/2020

--- color_changing/color_changing.py
+++ color_changing/color_changing.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2
 
 # read image
@@ -7,7 +6,10 @@ img = cv2.imread('image.jpg')
 print('img.shape:', img.shape)
 
 # replace blue channel with zero
-img[:,:,0] = 0
+img[:, :, 0] = 0
 
 # write / save modified image
 cv2.imwrite('image_yellow.jpg', img)
+
+cv2.imshow("image", img)  # show the image
+cv2.waitKey(0)  # wait until press any key
